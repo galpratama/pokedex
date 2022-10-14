dcecafc5df714353fcf64f4911a9ec817f2f1ab5
Galih Pratama <hanamura.iost@gmail.com>
2022-10-14 13:41:18 +0700

    feat(pokedex): list of pokemons

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,38 @@
 <script setup lang="ts">
-import { RouterView } from "vue-router";
+import { ref } from "vue";
+
+const activeIndex = ref("1");
+const handleSelect = (key: string, keyPath: string[]) => {
+  console.log(key, keyPath);
+};
 </script>
 
 <template>
+  <el-menu
+    :default-active="activeIndex"
+    class="el-menu-demo"
+    mode="horizontal"
+    :ellipsis="false"
+    @select="handleSelect"
+  >
+    <el-menu-item index="0">
+      <RouterLink to="/">
+        <img src="./assets/img/logo.svg" alt="" class="h-8 m-3" />
+      </RouterLink>
+    </el-menu-item>
+    <div class="flex-grow" />
+    <el-menu-item index="1">
+      <RouterLink to="/">Home</RouterLink>
+    </el-menu-item>
+    <el-menu-item index="2">
+      <RouterLink to="/about">About</RouterLink>
+    </el-menu-item>
+  </el-menu>
   <RouterView />
 </template>
 
-<style scoped></style>
+<style>
+.flex-grow {
+  flex-grow: 1;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,70 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+type Pokemon = {
+  name: string;
+  url: string;
+};
+
+const pokemons = ref<Pokemon[]>([]);
+const offset = ref(0);
+
+const fetchPokemon = async ({
+  offset = 0,
+  limit = 24,
+}: {
+  offset?: number;
+  limit?: number;
+}) => {
+  try {
+    const response = await fetch(
+      `https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`
+    );
+    const data = await response.json();
+    pokemons.value = data.results;
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getPokemonId = (url: string) => {
+  const id = url.split("/")[6];
+  return id;
+};
+
+onMounted(() => {
+  fetchPokemon({ offset: offset.value });
+});
+
+// Data
+</script>
 
 <template>
-  <main></main>
+  <main class="container mx-auto my-10">
+    <el-row>
+      <el-col
+        v-for="(pokemon, index) in pokemons"
+        infinite-scroll-delay="1000"
+        :key="index"
+        :xs="8"
+        :sm="6"
+        :md="4"
+        :lg="3"
+        :xl="1"
+      >
+        <el-card class="m-2 rounded-xl">
+          <div class="p-4 text-center">
+            <img
+              class="h-24 mx-auto mb-8"
+              :src="`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${getPokemonId(
+                pokemon.url
+              )}.svg`"
+              :alt="pokemon.name"
+            />
+            <span>{{ pokemon.name }}</span>
+          </div>
+        </el-card>
+      </el-col>
+    </el-row>
+  </main>
 </template>
