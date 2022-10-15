a786dea4ffd9ce0eff520484f2574594116a0532
Galih Pratama <hanamura.iost@gmail.com>
2022-10-15 12:33:59 +0700

    feat: compare page

--- src/pages/compare.vue
+++ src/pages/compare.vue
@@ -2,6 +2,7 @@
 
 <template>
   <main class="container mx-auto my-10">
-    <h1>This is a compare page for pokemon</h1>
+    <h1 class="text-2xl">Compare Pokemon</h1>
+    <p>You can compare two Pokemon here.</p>
   </main>
 </template>
