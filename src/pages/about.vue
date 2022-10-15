7f128d6ec677660f01d467ae0f463e614ea2b84c
Galih Pratama <hanamura.iost@gmail.com>
2022-10-15 12:02:49 +0700

    feat: about page

--- src/pages/about.vue
+++ src/pages/about.vue
@@ -1,5 +1,17 @@
 <template>
   <main class="container mx-auto my-10">
-    <h1>This is an about page</h1>
+    <h1 class="text-2xl">About Pokedex</h1>
+    <p>
+      This is a Pokedex built with Vue 3, TypeScript, and Tailwind CSS. It uses
+      the
+      <a
+        href="https://pokeapi.co/"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="text-blue-500"
+        >PokeAPI</a
+      >
+      to fetch data using GraphQL and Apollo Client for the homepage, and using Axios for the Pokemon page.
+    </p>
   </main>
 </template>
